c5d9aee28b237b8e0b946ceaa0351b8903494dfb
thbob2 <33073278+thbob2@users.noreply.github.com>
2021-07-11 22:09:23 +0100

    review must proceed implemented mister robot shity execption saver

---
+++ python/lexor.py
@@ -0,0 +1,24 @@
+from json.decoder import JSONDecodeError
+import regex as re
+import pickle
+import json
+import pandas as pd
+from textblob import TextBlob
+from twitter_client import *
+from Matweet import Tweet
+from os import error, listdir
+from os.path import isfile, join
+import os 
+import datetime as dt
+import exceptionsaver as Es
+
+def lexicalo(pathlist):
+    #! TODO fill the blancs
+    #? the main objective is to lexicaly analyse all tweet files in the path list
+    pass
+
+
+
+def Semantico(pathlist):
+    #TODO complement the previous lexicalo by a more elaborate analysis
+    pass
