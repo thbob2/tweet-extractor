829f24026592a3fcf14c74d91be6380076694ca4
chouaib <33073278+thbob2@users.noreply.github.com>
2021-09-07 21:56:34 +0100

    dumies

--- python/src/dumy_gen.py
+++ python/src/dumy_gen.py
@@ -3,9 +3,10 @@ import json
 import os
 from filtor import readJson
 import random
+import matplotlib.pyplot as plt
 def dummer():
-    dumy_dir1 = os.getcwd()+"/python/corp/data2.0/companies/NZXT/"
-    dumy_dates = []
+    dumy_dir1 = os.getcwd()+"/python/corp/data2.0/companies/RADEON/"
+    
     dirs = os.listdir(dumy_dir1) 
     dummies = []
     for d in dirs:
@@ -32,16 +33,21 @@ def dummer():
         })
 
     for dumm in dummies :
-        if(dumm['neg']*100/dumm['total']>=35.0) and dumm['neg'] >= 200:
+        if(dumm['neg']*100/dumm['total']>=20.0) and dumm['neg'] >=150:
             dumm['sales'] = random.randint(20000,30000)
         else:
             dumm['sales'] = random.randint(80000,90000)
+        #dumm['sales'] = random.randint(90000,100000)
 
-    with open(os.getcwd()+"/python/corp/dumm-nzxt.json","w") as writor:
+    with open(os.getcwd()+"/python/corp/dumm-radeon.json","w") as writor:
         json.dump({'dummies':[o for o in dummies]},writor,indent=4,ensure_ascii=False)
 
+def ploter():
+    dumm = readJson(os.getcwd()+"/python/corp/dumm-radeon.json")
+    plt.plot([o['date'] for o in dumm['dummies']],[o['neg'] for o in dumm['dummies']],[o['sales']/1000 for o in dumm['dummies']])
+    plt.show()
 if __name__ == '__main__':
-    n  = random.randint(80000,90000)
-    print(n)
     dummer()
+    ploter()
+
     
